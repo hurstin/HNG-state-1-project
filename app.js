23c9f3421ca231697060b9aeadb503bbb623fe86
hurstin <okechukwuhurstin@gmail.com>
2024-07-02 01:40:00 +0100

    commit

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use((req, res, next) => {
+  res.send('hello from the server side');
   console.log('hello from the middleware');
   next();
 });
